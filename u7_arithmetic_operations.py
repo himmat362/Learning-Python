8855fac5949df93faf85ece26133b17e1d38b048
himmat362 <107528345+himmat362@users.noreply.github.com>
2024-02-18 22:06:24 +0530

    Add files via upload

---
+++ u7_arithmetic_operations.py
@@ -0,0 +1,27 @@
+print(10+6)
+print(10/7)
+print(10//7)
+print(10**6)
+x=10
+x=x+3
+print(x)
+x=10
+x+=3
+print(x)
+x=10
+x=10+3*2
+print(x)
+order_of_operations='''brackets ()"''"
+exponentiation **
+multiplication * then division / or //
+addition + then substraction -'''
+print(order_of_operations)
+x=10
+x=10+3*2**2
+print(x)
+x=10
+x=10+(3*2)**2
+print(x)
+y=5.72
+print(round(y))
+print(abs(-94.1)) #abs is modulus func
