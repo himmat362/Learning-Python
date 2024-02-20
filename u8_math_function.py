c1384db889af3d4412f1700e2e2afeb63d8df85a
himmat362 <107528345+himmat362@users.noreply.github.com>
2024-02-20 14:33:48 +0530

    Add files via upload

---
+++ u8_math_function.py
@@ -0,0 +1,5 @@
+import math
+print(math.ceil(7541.36))
+print(math.floor(7541.36))
+print(math.comb(5,3)) #comb is for 5C3 type combinations
+print('search math functions in python 3 for all functions')
