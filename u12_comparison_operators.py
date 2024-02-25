94e9340d03710779168c27304e68e4c508f87475
himmat362 <107528345+himmat362@users.noreply.github.com>
2024-02-25 22:51:24 +0530

    Add files via upload

---
+++ u12_comparison_operators.py
@@ -0,0 +1,7 @@
+name="Lemvtg"
+if len(name)<3:
+    print('Name msut be atleast 3 characters')
+elif len(name)>30:
+    print('Name msut be maximum of 30 characters')
+else:
+    print('Name looks good')
