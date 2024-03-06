07d0717c67e816b3badf5c4e2ad4fa106b3aaf66
himmat362 <107528345+himmat362@users.noreply.github.com>
2024-03-06 23:34:03 +0530

    Add files via upload

---
+++ u17_for_loop.py
@@ -0,0 +1,9 @@
+names=["HImmat","Sehaj","Abhijot","Ankush","Armaan","Manamrit"]
+for name in names:
+    print(name)
+
+sum=0
+for num in range(1,100,10):
+    print(num, end='\t')
+    sum+=num
+print(f"\nTotal={sum}")
